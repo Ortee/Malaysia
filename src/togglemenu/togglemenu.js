a50c48510c049d1ddc87305dba3ad5ee78836cee
Mateusz <kontakt@mateuszoracz.pl>
2016-08-02 20:09:16 +0200

    menu bootstrap refactore

--- src/togglemenu/togglemenu.js
+++ src/togglemenu/togglemenu.js
@@ -11,20 +11,20 @@ export default class ToggleMenu extends React.Component {
   render() {
     return(
         <div class="row">
-          <div class="col-xs-8 col-sm-8 col-md-8 col-xs-offset-2 col-sm-offset-2 col-md-offset-2 toggleMenu">
-            <div class="col-xs-2 col-sm-2 col-md-2 toggleMenuButton">
+          <div class="col-xs-8 col-xs-offset-2 toggleMenu">
+            <div class="col-xs-2 toggleMenuButton">
               <Link to={'/'}><i class="fa fa-arrow-left fa-lg" aria-hidden="true"></i></Link>
             </div>
-            <div class="col-xs-2 col-sm-2 col-md-2 toggleMenuButton">
+            <div class="col-xs-2 toggleMenuButton">
               <Link to={'/news'}><i class="fa fa-plane fa-lg" aria-hidden="true"></i></Link>
             </div>
-            <div class="col-xs-2 col-sm-2 col-md-2 toggleMenuButton">
+            <div class="col-xs-2 toggleMenuButton">
               <Link to={'/calendar'}><i class="fa fa-calendar fa-lg" aria-hidden="true"></i></Link>
             </div>
-            <div class="col-xs-2 col-sm-2 col-md-2 toggleMenuButton">
+            <div class="col-xs-2 toggleMenuButton">
               <Link to={'/galery'}><i class="fa fa-camera fa-lg" aria-hidden="true"></i></Link>
             </div>
-            <div class="col-xs-2 col-sm-2 col-md-2 toggleMenuButton">
+            <div class="col-xs-2 toggleMenuButton">
               <Link to={'/documents'}><i class="fa fa-info fa-lg" aria-hidden="true"></i></Link>
             </div>
           </div>
