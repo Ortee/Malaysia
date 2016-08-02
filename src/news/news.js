b4d69e5ebdd5598fc030ee42cf9e991745f23abc
Mateusz <kontakt@mateuszoracz.pl>
2016-08-02 20:09:59 +0200

    responsive fix

--- src/news/news.js
+++ src/news/news.js
@@ -15,8 +15,8 @@ export default class News extends React.Component {
       <div class="container">
       <ToggleMenu></ToggleMenu>
         <div class="row">
-          <div class="col-xs-8 col-sm-8 col-md-8 col-md-offset-2 pageBackground">
-            <div class="col-md-10 col-md-offset-1 pageLogo">
+          <div class="col-xs-12 col-md-8 col-md-offset-2 pageBackground">
+            <div class="col-xs-10 col-xs-offset-1 pageLogo">
               AKTUALNOSCI
             </div>
             <NewsList></NewsList>
--- src/news/newsPost.js
+++ src/news/newsPost.js
@@ -10,10 +10,10 @@ export default class NewsPost extends React.Component {
   render() {
     return(
       <div class="row">
-        <div class="col-xs-2 col-sm-2 col-md-2 eventDate">
+        <div class="col-xs-2 eventDate">
           {this.props.eventDate}
         </div>
-        <div class="col-xs-10 col-sm-10 col-md-10 eventConent">
+        <div class="col-xs-10 eventConent">
           <p class="eventTitle">{this.props.eventTitle}</p>
           <p class="eventText">{this.props.eventContent}</p>
           <p class="readMore">czytaj dalej...</p>
