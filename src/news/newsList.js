327621b70d9eb0e1a03f127704533d7fcab54172
Mateusz <kontakt@mateuszoracz.pl>
2016-08-02 20:41:02 +0200

    posts from posts.json file

--- src/news/newsList.js
+++ src/news/newsList.js
@@ -6,17 +6,30 @@ import NewsPost from './newsPost';
 export default class NewsList extends React.Component {
   constructor(){
     super();
+    this.state = { posts: false };
   }
+
+  componentDidMount(){
+    $.getJSON(__dirname + 'posts.json').done(function(data) {
+      this.setState({posts: data.posts});
+    }.bind(this));
+  }
+
   render() {
-    return(
-      <artice>
-        <div id="eventList" class="col-xs-12 col-sm-12 col-md-12 ">
-            <NewsPost eventDate="01-01-2016" eventTitle="Wydarzenie 1" eventContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat pharetra nibh vitae efficitur. Morbi sit amet luctus libero, sit amet molestie tellus. Nunc auctor sit amet enim in egestas. Proin eget consequat risus, nec placerat lacus"></NewsPost>
-            <NewsPost eventDate="02-01-2016" eventTitle="Wydarzenie 2" eventContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat pharetra nibh vitae efficitur. Morbi sit amet luctus libero, sit amet molestie tellus."></NewsPost>
-            <NewsPost eventDate="03-01-2016" eventTitle="Wydarzenie 3" eventContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat pharetra nibh vitae efficitur. Morbi sit amet luctus libero, sit amet molestie tellus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat pharetra nibh vitae efficitur. Morbi sit amet luctus libero, sit amet molestie tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat pharetra nibh vitae efficitur. Morbi sit amet luctus libero, sit amet molestie tellus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat pharetra nibh vitae efficitur. Morbi sit amet luctus libero, sit amet molestie tellus."></NewsPost>
-            <NewsPost eventDate="04-01-2016" eventTitle="Wydarzenie 4" eventContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat pharetra nibh vitae efficitur. Morbi sit amet luctus libero, sit amet molestie tellus."></NewsPost>
-          </div>
-      </artice>
-    );
+    let rows = [];
+    if(this.state.posts)
+    {
+      this.state.posts.forEach(function(post, index) {
+        rows.push(<NewsPost key={index} eventDate={post.date} eventTitle={post.title} eventContent={post.shortDescription}></NewsPost>);
+       });
+      return(
+        <artice>
+          <div id="eventList" class="col-xs-12">
+              {rows}
+            </div>
+        </artice>
+      );
+    }
+    return(<div></div>);
   }
 }
